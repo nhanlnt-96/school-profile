18c5513d2324e0955b40c51cc749dfcf86465189
Lnt.Nhan <nhanlnt@hotmail.com>
2021-03-28 01:15:42 +0700

    upload project to repo

---
+++ README.md
@@ -0,0 +1,5 @@
+### SOURCE DESIGN
+
+''''
+https://www.figma.com/file/VdIHpavZguVymcuYhBdR3z/TanVan?node-id=0%3A1
+''''
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header } from './containers/Header';
+
+import './style/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <Header/>
+      </div>
+      <div className="container">
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
